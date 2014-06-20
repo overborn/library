f6c8e0195623d89f1294fefcdfc6f4724575968c
overborn <forkirill@i.ua>
2014-06-21 02:04:11 +0300

    7

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web: python run.py
+web: gunicorn run-heroku:app
+init: python db_create.py
+upgrade: python db_upgrade.py
--- config.py
+++ config.py
@@ -4,10 +4,16 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-WHOOSH_BASE = os.path.join(basedir, 'search.db')
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') + '?check_same_thread=False'
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_RECORD_QUERIES = True
+
+WHOOSH_BASE = os.path.join(basedir, 'search.db')
+# Whoosh does not work on Heroku
+WHOOSH_ENABLED = os.environ.get('HEROKU') is None
 
 CSRF_ENABLED = True
 SECRET_KEY = '\x84iiDQ\x82\x875\xc2\x9d\x13\x11\xfd\xb4\xa2\x91\x95\x85v\xf6o\xa3<\x9f'
