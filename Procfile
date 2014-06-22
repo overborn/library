5f52392d6afb006163f2b45599bda6528f80fc7b
overborn <forkirill@i.ua>
2014-06-22 16:55:21 +0300

    7

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 web: gunicorn run-heroku:app
 init: python db_create.py
 upgrade: python db_upgrade.py
+db-fill: python fill-db.py
--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,13 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 
+if os.environ.get('HEROKU') is not None:
+    import logging
+    stream_handler = logging.StreamHandler()
+    app.logger.addHandler(stream_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('microblog startup')
+
 from app import views, models
 
 
